135a7ec59e865261e358d6c7cd914141c68e27cf
Jesse Iberri <jesseiberri@gmail.com>
2022-04-28 12:01:30 -0700

    Commit for 1-4 completed exercises

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(myArray, ...myValues) {
+    let newArray = [];
+    
+    for (let i = 0; i < myArray.length; i++) {
+        let element = myArray[i];
+        if (!myValues.includes(element)) newArray.push(element);
+    }
 
+    return newArray;
 };
 
 // Do not edit below this line

fe62e8725af03850281136441529efcaaa4e784a
Jesse Iberri <jesseiberri@gmail.com>
2022-05-02 11:27:46 -0700

    Completed Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  /* [°C] = ([°F] − 32) × 5⁄9 */
+  let c = ((f - 32) * (5/9));
+  c = parseFloat(c.toFixed(1));
+  return c;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  /*[°F] = [°C] × 9⁄5 + 32 */
+  let f = (c * (9 / 5) + 32);
+  f = parseFloat(f.toFixed(1));
+  return f;
 };
 
 // Do not edit below this line

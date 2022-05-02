b3fe664645cdc09f7ed57e0e77a9098cc0fb8d4c
Jesse Iberri <jesseiberri@gmail.com>
2022-05-02 10:56:30 -0700

    Completed Exercise 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    for (let arg of arguments) {
+        if (isNaN(arg) || typeof(arg) != 'number' || arg < 0) return 'ERROR';
+    }
 
+    let start = Math.min(num1, num2);
+    let end = Math.max(num1, num2);
+    let sum = 0;
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line

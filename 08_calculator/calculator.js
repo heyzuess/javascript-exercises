9c4a2fe0b87d6a542510cca1c4c6a88f1443f1c3
Jesse Iberri <jesseiberri@gmail.com>
2022-05-23 14:52:40 -0700

    Completed Exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,35 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(list) {
+  let value = list.reduce((sum, item) => {
+    return sum += item;
+  }, 0);
+  return value;
 };
 
-const multiply = function() {
-
+const multiply = function(list) {
+  let value = list.reduce((sum, item) => {
+    return sum *= item;
+  });
+  return value;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(start) {
+  let val = 1;
+  for (let i = start; i > 0; i--) {
+    val *= i;
+  }
+  return val;
 };
 
 // Do not edit below this line

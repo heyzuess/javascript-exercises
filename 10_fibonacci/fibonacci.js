f3924dbd1dad516ae38ed80fc1a68fd3d09b4f5f
Jesse Iberri <jesseiberri@gmail.com>
2022-05-25 11:41:36 -0700

    Completed Exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
+const fibonacci = function(size) {
+    if (size < 0) return "OOPS";
 
+    let length = parseInt(size);
+    length--;
+
+    let numbers = [1,];
+    let current = numbers[0];
+    
+    for (let i = 0; i < length; i++) {
+        let prev = i <= 0 ? 0 : numbers[i-1];
+        current += prev;
+        numbers.push(current);
+    }
+
+    return numbers[length];
 };
 
 // Do not edit below this line

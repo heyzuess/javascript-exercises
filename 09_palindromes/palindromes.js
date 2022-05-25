03ee21605b8f7c69f7bd5eff39e436b3b3b604fe
Jesse Iberri <jesseiberri@gmail.com>
2022-05-25 11:09:59 -0700

    Completed Exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,30 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let myWord = filterWord(word);
+    let reverse = reverseString(myWord);
+    return reverse === myWord;
 };
+
+function filterWord (string) {
+    string = string.toLowerCase();
+
+    let filterOut = [
+        '!', ',', "'", '"', '.', '?', ' '
+    ]
+
+    for (let filter of filterOut) {
+        string = string.replaceAll(filter, '');
+    }
+
+    return string;
+}
+
+function reverseString (string) {
+    let val = "";
+    for (let i = string.length - 1; i >= 0; i--) {
+        val += string[i];
+    }
+    return val;
+}
 
 // Do not edit below this line
 module.exports = palindromes;

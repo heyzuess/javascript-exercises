3d5329def2b3b19f08533ecd8947574df8238a49
Jesse Iberri <jesseiberri@gmail.com>
2022-05-26 18:06:53 -0700

    Completed Exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let getAge = (obj) => {
+        let yearOfDeath = obj.yearOfDeath ?? (new Date()).getFullYear();
+        let val = yearOfDeath - obj.yearOfBirth;
+        return val;
+    }
 
+    const oldToYoung = people.sort((a, b) => {
+        let ageA = getAge(a);
+        let ageB = getAge(b);
+
+        return ageA > ageB ? -1 : 1;
+    });
+
+    people.name = oldToYoung[0].name;
+    return people;
 };
 
 // Do not edit below this line
